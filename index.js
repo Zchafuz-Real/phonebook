abf5ef0e1c79e567a7ef3ff9d784c5dba71c63c5
filip <s220257@dtu.dk>
2023-03-31 15:46:13 +0200

    with build

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ morgan.token('person', function getP (req) {
     return null
 })
 
-
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :person'))
 
